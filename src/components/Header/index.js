70cb5e101d92eafa90bde16730aaf59dfa850e77
RL Brown <rlbrown72@gmail.com>
2018-08-10 11:38:35 -0400

    top margin

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import { flicker } from '../../utils/styles';
 
 const HeaderText = styled.div`
   font-size: 1.5rem;
-  margin-bottom: 4.5rem;
+  margin: 1rem 0 4.5rem 0;
   animation: ${flicker} .0101s linear infinite;
 `;
 
